bf5c895a5d6b4e1b82f32af89f6b37230d1e2379
Jeremiah Oertling <jeremiah.oertling@intralox.com>
2023-05-10 19:42:52 -0500

    Create config_TEMPLATE.py
    use this as a starting point for all of your keys

---
+++ config_TEMPLATE.py
@@ -0,0 +1,11 @@
+OPENAI_API_KEY = ""
+DYNLIST_API_KEY = ""
+
+# App setups
+LLM_MODEL = "gpt-3.5-turbo"
+
+# Test setups
+OPENAI_ASSISTANT_SETUP = "You are a transcription assistant. I will give you an unformatted "
+OPENAI_ASSISTANT_SETUP += "text string, and you will return the string formatted with "
+OPENAI_ASSISTANT_SETUP += "punctionation and likely misspelled words corrected:"
+TEST_TRANSCRIPTION = "this is a recording we are testing a python library called nemo hello world"
