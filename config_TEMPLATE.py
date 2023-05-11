fa37c07e07629ed70e508e8e4bc6f9a5f02f468d
Jeremiah Oertling <jeremiah.oertling@intralox.com>
2023-05-11 08:17:21 -0500

    Update config_TEMPLATE.py
    moved all config parameters to config.py

--- config_TEMPLATE.py
+++ config_TEMPLATE.py
@@ -2,10 +2,17 @@ OPENAI_API_KEY = ""
 DYNLIST_API_KEY = ""
 
 # App setups
-LLM_MODEL = "gpt-3.5-turbo"
+## NeMo
+NEMO_ASR_MODEL="QuartzNet15x5Base-En"
+
+## OPENAI
+OPENAI_LLM_MODEL = "gpt-3.5-turbo"
+
+## Dynalist
+DYNALIST_TAGS = ["#scribe"]
 
 # Test setups
 OPENAI_ASSISTANT_SETUP = "You are a transcription assistant. I will give you an unformatted "
 OPENAI_ASSISTANT_SETUP += "text string, and you will return the string formatted with "
-OPENAI_ASSISTANT_SETUP += "punctionation and likely misspelled words corrected:"
+OPENAI_ASSISTANT_SETUP += "punctionation and likely misspelled words corrected. Interpret the word "'hashtag'" as the symbol #:"
 TEST_TRANSCRIPTION = "this is a recording we are testing a python library called nemo hello world"
